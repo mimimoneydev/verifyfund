5fc019b5474870f277c4ecd555e9968708e9be5a
Mark <mimimoneyapp@gmail.com>
2025-10-28 14:58:41 +0300

    Add files via upload

---
+++ verifund-client/next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "gateway.pinata.cloud",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
